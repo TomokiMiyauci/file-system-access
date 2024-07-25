9af1f6d6561c0284c3ffe8e0e8df621cabb12a12
Tomoki Miyauchi <development.operation.6.6.6@gmail.com>
2024-07-25 23:27:01 +0900

    chore(type): add shared types

---
+++ src/type.ts
@@ -0,0 +1,108 @@
+interface BaseEntry {
+  /**
+   * @see https://fs.spec.whatwg.org/#entry-name
+   */
+  name: string;
+
+  /**
+   * @see https://fs.spec.whatwg.org/#entry-query-access
+   */
+  queryAccess(mode: "read" | "readwrite"): FileSystemAccessResult;
+
+  /**
+   * @see https://fs.spec.whatwg.org/#entry-request-access
+   */
+  requestAccess(mode: "read" | "readwrite"): FileSystemAccessResult;
+}
+
+export interface FileEntry extends BaseEntry {
+  /**
+   * @see https://fs.spec.whatwg.org/#file-entry-binary-data
+   */
+  binaryData: ArrayBuffer;
+
+  /** A number representing the number of milliseconds since the Unix Epoch.
+   * @see https://fs.spec.whatwg.org/#file-entry-modification-timestamp
+   */
+  modificationTimestamp: number;
+
+  /**
+   * @see https://fs.spec.whatwg.org/#file-entry-lock
+   */
+  lock: "open" | "taken-exclusive" | "taken-shared";
+
+  /** A number representing the number shared locks that are taken at a given point in time
+   * @see https://fs.spec.whatwg.org/#file-entry-shared-lock-count
+   */
+  sharedLockCount: number;
+}
+
+export interface DirectoryEntry extends BaseEntry {
+  /** File system entries.
+   * @see https://fs.spec.whatwg.org/#directory-entry-children
+   */
+  children: FileSystemEntry[];
+}
+
+/**
+ * @see https://fs.spec.whatwg.org/#entry
+ */
+export type FileSystemEntry = FileEntry | DirectoryEntry;
+
+/** Struct encapsulating the result of {@link BaseEntry.queryAccess querying} or {@link BaseEntry.requestAccess requesting} access to the file system.
+ * @see https://fs.spec.whatwg.org/#file-system-access-result
+ */
+export interface FileSystemAccessResult {
+  /**
+   * @see https://fs.spec.whatwg.org/#file-system-access-result-permission-state
+   */
+  permissionState: PermissionState;
+
+  /** A string which must be the empty string if {@link permissionState permission state} is "granted"; otherwise an name listed in the DOMException names table. It is expected that in most cases when {@link permissionState permission state} is not "granted", this should be "NotAllowedError".
+   * @see https://fs.spec.whatwg.org/#file-system-access-result-error-name
+   */
+  errorName: string;
+}
+
+/** A potential location of a {@link FileSystemEntry file system entry}.
+ *
+ * @see https://fs.spec.whatwg.org/#file-system-locator
+ */
+export type FileSystemLocator = FileLocator | DirectoryLocator;
+
+interface BaseLocator {
+  /**
+   * @see https://fs.spec.whatwg.org/#locator-path
+   */
+  path: string[];
+
+  /**
+   * @see https://fs.spec.whatwg.org/#locator-kind
+   */
+  kind: FileSystemHandleKind;
+
+  /**
+   * @see https://fs.spec.whatwg.org/#locator-root
+   */
+  root: string;
+}
+
+/**
+ * @see https://fs.spec.whatwg.org/#directory-locator
+ */
+export interface DirectoryLocator extends BaseLocator {
+  /**
+   * @see https://fs.spec.whatwg.org/#locator-kind
+   */
+  kind: "directory";
+}
+
+/**
+ * @see https://fs.spec.whatwg.org/#file-locator
+ */
+export interface FileLocator extends BaseLocator {
+  /**
+   * @see https://fs.spec.whatwg.org/#locator-kind
+   */
+  kind: "file";
+}
