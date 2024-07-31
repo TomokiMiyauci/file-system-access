e9c199aafbabc0777095aef23f11ae983db1e6f5
Tomoki Miyauchi <development.operation.6.6.6@gmail.com>
2024-07-31 16:18:12 +0900

    refactor(binding): split ffi symbol

---
+++ src/deno/binding.ts
@@ -0,0 +1,53 @@
+export const symbols = {
+  __Dialog_new: {
+    parameters: [],
+    result: "pointer",
+    nonblocking: false,
+  },
+  __Dialog_pick_file: {
+    parameters: [
+      "pointer",
+    ],
+    result: "pointer",
+    nonblocking: false,
+  },
+  __Dialog_pick_files: {
+    parameters: [
+      "pointer",
+    ],
+    result: "pointer",
+    nonblocking: false,
+  },
+  __Dialog_pick_directory: {
+    parameters: [
+      "pointer",
+    ],
+    result: "pointer",
+    nonblocking: false,
+  },
+  __Dialog_set_directory: {
+    parameters: [
+      "pointer",
+      "buffer",
+      "usize",
+    ],
+    result: "pointer",
+    nonblocking: false,
+  },
+  __Dialog_add_filter: {
+    parameters: [
+      "pointer",
+      "buffer",
+      "usize",
+    ],
+    result: "pointer",
+    nonblocking: false,
+  },
+  __Dialog_dealloc: {
+    parameters: [
+      "pointer",
+    ],
+    result: "void",
+    nonblocking: false,
+  },
+} satisfies Deno.ForeignLibraryInterface;
