774542c2adfb8941feb4c127428edf9ef60bfbf6
Tomoki Miyauchi <development.operation.6.6.6@gmail.com>
2024-08-30 23:33:45 +0900

    feat(mod): export related type

--- src/mod.ts
+++ src/mod.ts
@@ -1,4 +1,7 @@
-export { createFileSystemAccess } from "./file_system_access.ts";
+export {
+  createFileSystemAccess,
+  type FileSystemAccess,
+} from "./file_system_access.ts";
 export { type UserAgent } from "./implementation_defined.ts";
 export * from "./type.ts";
 export { validateSuffix } from "./algorithm.ts";
