8989bcca54a1f38e13e407d7a4249ef457ff6568
Tomoki Miyauchi <development.operation.6.6.6@gmail.com>
2024-08-30 23:26:16 +0900

    refactor(adaptor): merge functions into class

--- src/node/adaptor.ts
+++ src/node/adaptor.ts
@@ -7,8 +7,6 @@ import { homedir } from "node:os";
 import type { FileSystemEntry, FileSystemPath } from "@miyauci/fs";
 import type {
   FileLocation,
-  OpenDirectoryPicker,
-  OpenFileDialog,
   OpenFileDialogOptions,
   OpenSaveFilePickerOptions,
   Options,
@@ -17,126 +15,117 @@ import type {
 } from "../implementation_defined.ts";
 
 export class UserAgent implements IUserAgent {
-  openFileDialog: OpenFileDialog = openFileDialog;
-  openDirectoryDialog: OpenDirectoryPicker = openDirectoryDialog;
+  openFileDialog(options: OpenFileDialogOptions): FileLocation[] | null {
+    const fileDialog = new FileDialog();
+
+    if (options.startingDirectory) {
+      fileDialog.setDirectory(options.startingDirectory);
+    }
+
+    for (const [description, exts] of options.acceptsOptions) {
+      const extsWithoutDot = exts.map((ext) =>
+        ext.startsWith(".") ? ext.slice(1) : ext
+      );
+
+      fileDialog.addFilter(description, extsWithoutDot);
+    }
+
+    if (options?.multiple) {
+      const paths = fileDialog.pickFiles();
+
+      if (!paths) return null;
+
+      return paths.map(toLoc);
+    }
+
+    const fullPath = fileDialog.pickFile();
+
+    if (!fullPath) return null;
+
+    return [toLoc(fullPath)];
+  }
+  openDirectoryDialog(options: Options): FileLocation | null {
+    const fileDialog = new FileDialog();
+
+    if (options.startingDirectory) {
+      fileDialog.setDirectory(options.startingDirectory);
+    }
+
+    const fullPath = fileDialog.pickFolder();
+
+    if (!fullPath) return null;
+
+    return toLoc(fullPath);
+  }
+
+  openSaveFileDialog(options: OpenSaveFilePickerOptions): FileLocation | null {
+    const dialog = new FileDialog();
+
+    if (options.startingDirectory) {
+      dialog.setDirectory(options.startingDirectory);
+    }
+
+    if (options?.suggestedName) dialog.setFileName(options.suggestedName);
+
+    for (const [description, exts] of options.acceptsOptions) {
+      const extsWithoutDot = exts.map((ext) =>
+        ext.startsWith(".") ? ext.slice(1) : ext
+      );
+
+      dialog.addFilter(description, extsWithoutDot);
+    }
+
+    const path = dialog.saveFile();
+
+    if (!path) return null;
+
+    writeFileSync(path, new Uint8Array());
+
+    return toLoc(path);
+  }
 
   locateEntry(root: string, path: FileSystemPath): FileSystemEntry | null {
     const fs = new BucketFileSystem(root);
 
     return fs.locateEntry(path);
   }
-  openSaveFileDialog = openSaveFileDialog;
   wellKnownDirectories: IWellKnownDirectoryMap = new WellKnownDirectoryMap();
   recentlyPickedDirectoryMap: Map<unknown, Map<string, string>> = new Map();
   defaultPath: string = "";
 }
 
 class WellKnownDirectoryMap implements IWellKnownDirectoryMap {
-  get desktop() {
-    return join(homedir(), "Desktop");
+  get #home(): string {
+    return homedir();
   }
 
-  get documents() {
-    return join(homedir(), "Documents");
+  get desktop(): string {
+    return join(this.#home, "Desktop");
   }
 
-  get downloads() {
-    return join(homedir(), "Downloads");
+  get documents(): string {
+    return join(this.#home, "Documents");
   }
 
-  get music() {
-    return join(homedir(), "Music");
+  get downloads(): string {
+    return join(this.#home, "Downloads");
   }
 
-  get pictures() {
-    return join(homedir(), "Pictures");
+  get music(): string {
+    return join(this.#home, "Music");
   }
 
-  get videos() {
-    return join(homedir(), "Videos");
-  }
-}
-
-export function openFileDialog(
-  options: OpenFileDialogOptions,
-): FileLocation[] | null {
-  const fileDialog = new FileDialog();
-
-  if (options.startingDirectory) {
-    fileDialog.setDirectory(options.startingDirectory);
+  get pictures(): string {
+    return join(this.#home, "Pictures");
   }
 
-  for (const [description, exts] of options.acceptsOptions) {
-    const extsWithoutDot = exts.map((ext) =>
-      ext.startsWith(".") ? ext.slice(1) : ext
-    );
-
-    fileDialog.addFilter(description, extsWithoutDot);
+  get videos(): string {
+    return join(this.#home, "Videos");
   }
-
-  if (options?.multiple) {
-    const paths = fileDialog.pickFiles();
-
-    if (!paths) return null;
-
-    return paths.map(toLoc);
-  }
-
-  const fullPath = fileDialog.pickFile();
-
-  if (!fullPath) return null;
-
-  return [toLoc(fullPath)];
-}
-
-export function openSaveFileDialog(
-  options: OpenSaveFilePickerOptions,
-): FileLocation | null {
-  const fileDialog = new FileDialog();
-
-  if (options.startingDirectory) {
-    fileDialog.setDirectory(options.startingDirectory);
-  }
-
-  if (options?.suggestedName) {
-    fileDialog.setFileName(options.suggestedName);
-  }
-
-  for (const [description, exts] of options.acceptsOptions) {
-    const extsWithoutDot = exts.map((ext) =>
-      ext.startsWith(".") ? ext.slice(1) : ext
-    );
-
-    fileDialog.addFilter(description, extsWithoutDot);
-  }
-
-  const path = fileDialog.saveFile();
-
-  if (!path) return null;
-
-  writeFileSync(path, new Uint8Array());
-
-  return toLoc(path);
 }
 
 function toLoc(path: string): FileLocation {
   const { base: name, dir: root } = parse(path);
 
   return { name, root };
-}
-
-export function openDirectoryDialog(
-  options: Options,
-): FileLocation | null {
-  const fileDialog = new FileDialog();
-
-  if (options.startingDirectory) {
-    fileDialog.setDirectory(options.startingDirectory);
-  }
-
-  const fullPath = fileDialog.pickFolder();
-
-  if (!fullPath) return null;
-
-  return toLoc(fullPath);
 }
