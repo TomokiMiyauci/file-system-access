ddc0012ae973aced2a6c2bf22876dcb1096109f4
Tomoki Miyauchi <development.operation.6.6.6@gmail.com>
2024-09-02 23:38:16 +0900

    docs: add description

--- src/node/mod.ts
+++ src/node/mod.ts
@@ -18,11 +18,17 @@ const environment = {
   userAgent: new UserAgent(),
 } satisfies Environment;
 
-/** Shows a file picker that lets a user select a single existing file, returning a handle for the selected file. */
+/** Shows a file picker that lets a user select a single existing file, returning a handle for the selected file.
+ *
+ * [File System Access](https://wicg.github.io/file-system-access/#dom-window-showopenfilepicker)
+ */
 export function showOpenFilePicker(
   options?: OpenFilePickerOptions & { multiple?: false },
 ): Promise<[FileSystemFileHandle]>;
-/** Shows a file picker that lets a user select multiple existing files, returning handles for the selected files. */
+/** Shows a file picker that lets a user select multiple existing files, returning handles for the selected files.
+ *
+ * [File System Access](https://wicg.github.io/file-system-access/#dom-window-showopenfilepicker)
+ */
 export function showOpenFilePicker(
   options?: OpenFilePickerOptions & { multiple?: true },
 ): Promise<FileSystemFileHandle[]>;
@@ -32,12 +38,22 @@ export function showOpenFilePicker(
   return showOpenFilePickerWith(environment, options);
 }
 
+/** Shows a directory picker that lets the user select a single directory, returning a handle for the selected directory if the user grants read permission.
+ *
+ * [File System Access](https://wicg.github.io/file-system-access/#dom-window-showdirectorypicker)
+ */
 export function showDirectoryPicker(
   options?: DirectoryPickerOptions,
 ): Promise<FileSystemDirectoryHandle> {
   return showDirectoryPickerWith(environment, options);
 }
 
+/** Shows a file picker that lets a user select a single file, returning a handle for the selected file.
+ *
+ * The selected file does not have to exist already. If the selected file does not exist a new empty file is created before this method returns, otherwise the existing file is cleared before this method returned.
+ *
+ * [File System Access](https://wicg.github.io/file-system-access/#dom-window-showsavefilepicker)
+ */
 export function showSaveFilePicker(
   options?: SaveFilePickerOptions,
 ): Promise<FileSystemFileHandle> {
